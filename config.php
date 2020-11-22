7402882d00dc74aa0ae0953ed07af4f84c9300b1
kush1999-h <74780178+kush1999-h@users.noreply.github.com>
2020-11-22 11:55:03 +0600

    FavJewels

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_Host","localhost");
+define("DB_user","sahoshib_fabjewels");
+define("DB_password","{LbK0(B.l7.o");
+define("DB_database","sahoshib_fav_jewels");
+
+$mysqli = new mysqli(DB_Host,DB_user,DB_password,DB_database);
+?>
