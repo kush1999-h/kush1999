880cb2e14686c7725c5d0dea81fc4d571774bfa6
kush1999-h <74780178+kush1999-h@users.noreply.github.com>
2020-11-22 12:20:10 +0600

    FabJewels

---
+++ register.php
@@ -0,0 +1,12 @@
+<?php
+$email = $_POST['E-mail'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if(!empty($email) || !empty($username) || !empty($password)){
+    $host=
+} else{
+    echo "All field are required";
+    die();
+}
+?>
